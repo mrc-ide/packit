ed8b9e0674f3e4e5a979655f1eed51a049273dc6
Lekan Anifowoshe <anney.lake@gmail.com>
2023-03-10 16:56:34 +0000

    add styling

--- app/src/app/App.tsx
+++ app/src/app/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Header from "./components/header/Header";
+import active from "../config/active.json";
 
 function App() {
     return (
-        <div className="App">
+        <div className={active.instance}>
             <Header/>
-            <div className="App-main">
+            <div className="app">
                 <p data-testid={"app-welcome"}>Welcome to packit.</p>
             </div>
         </div>
--- app/src/app/components/header/Header.tsx
+++ app/src/app/components/header/Header.tsx
@@ -1,9 +1,32 @@
 import React from "react";
+import active from "../../../config/active.json";
+import HelpIcon from "@mui/icons-material/Help";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
 const Header = () => {
     return (
         <header>
-            <p data-testid={"App-header"}>Test.</p>
+            <nav className="navbar navbar-light bg-light">
+                <a className="navbar-brand" href="/">
+                    <img src={active.logo}
+                         width="141"
+                         height="36"
+                         alt=""/></a>
+                <span>
+                    <span className="px-3">
+                        <a className="btn btn-sm btn-dark rounded"
+                           href="#">Accessibility</a>
+                    </span>
+                    <span className="px-3">
+                         <HelpIcon className="icon-primary" sx={{fontSize: 35}}/>
+                    </span>
+                    <span className="px-3">
+                          <AccountCircleIcon className="icon-primary" sx={{fontSize: 35}}/>
+                          <ArrowDropDownIcon className="icon-primary"/>
+                    </span>
+                </span>
+            </nav>
         </header>
     );
 };
