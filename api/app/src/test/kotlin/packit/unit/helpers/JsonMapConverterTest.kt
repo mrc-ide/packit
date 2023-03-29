7766b8e2c696dc3250449475d887b7332a3d7d90
Lekan Anifowoshe <anney.lake@gmail.com>
2023-03-29 11:53:50 +0100

    add converter test

---
+++ api/app/src/test/kotlin/packit/unit/helpers/JsonMapConverterTest.kt
@@ -0,0 +1,33 @@
+package packit.unit.helpers
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import packit.helpers.JsonMapConverter
+
+class JsonMapConverterTest
+{
+    private val objectMapper = ObjectMapper()
+
+    @Test
+    fun `converts map to string and vice versa`()
+    {
+        val paramsMap = mapOf(
+            "name" to "John",
+            "age" to 30,
+            "isEmployed" to true
+        )
+
+        val jsonParamsString = objectMapper.writeValueAsString(paramsMap)
+
+        val converter = JsonMapConverter(objectMapper)
+
+        val resultString = converter.convertToDatabaseColumn(paramsMap)
+
+        val resultMap = converter.convertToEntityAttribute(jsonParamsString)
+
+        assertEquals(jsonParamsString, resultString)
+
+        assertEquals(paramsMap, resultMap)
+    }
+}
