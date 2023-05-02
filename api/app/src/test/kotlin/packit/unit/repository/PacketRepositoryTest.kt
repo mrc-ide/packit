c1e4cf29d2cb332e895752083ec983acf3567bcc
Alex <alex.hill@gmail.com>
2023-05-02 14:23:26 +0100

    fix test style and add task to github actions

--- api/app/src/test/kotlin/packit/unit/repository/PacketRepositoryTest.kt
+++ api/app/src/test/kotlin/packit/unit/repository/PacketRepositoryTest.kt
@@ -3,7 +3,6 @@ package packit.unit.repository
 import jakarta.transaction.Transactional
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ActiveProfiles
@@ -34,5 +33,4 @@ class PacketRepositoryTest
 
         assertEquals(result, packet)
     }
-
 }
