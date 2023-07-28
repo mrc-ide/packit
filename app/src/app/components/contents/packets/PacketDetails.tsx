4214be1e78af807a06b65fb7afab88ad4aeafef8
Lekan Anifowoshe <anney.lake@gmail.com>
2023-07-28 11:42:11 +0100

    add full screen

--- app/src/app/components/contents/packets/PacketDetails.tsx
+++ app/src/app/components/contents/packets/PacketDetails.tsx
@@ -5,6 +5,7 @@ import ParameterList from "./ParameterList";
 import {useParams} from "react-router-dom";
 import {actions} from "../../../store/packets/thunks";
 import {PacketFile} from "./PacketFile";
+import appConfig from "../../../../config/appConfig";
 
 export default function PacketDetails() {
 
@@ -31,7 +32,13 @@ export default function PacketDetails() {
     const getHashOfHtmlFile = () => {
         return hasFiles()
             ? packet.files.find(file => file.path.split(".").pop() === "html")
-            : undefined;
+            : null;
+    };
+
+    const getPath = () => {
+        return getHashOfHtmlFile()
+            ? `packets/file/${getHashOfHtmlFile()?.hash}?inline=true&filename=${getHashOfHtmlFile()?.path}`
+            : null;
     };
 
     if (packetError) {
@@ -53,8 +60,17 @@ export default function PacketDetails() {
                 </span>
                 {hasParameters() && <ParameterList parameters={packet.parameters}/>}
             </div>
-            {<div data-testid="runner-content" className="content-box">
-                {hasFiles() && <PacketFile fileMetadata={getHashOfHtmlFile()}/>}
+            {getPath() && <div>
+                <div data-testid="runner-content" className="content-box">
+                    <PacketFile path={getPath()}/>
+                </div>
+                <div data-testid="view-fullscreen"
+                     className="p-4 d-flex flex-column align-items-end">
+                        <a className="btn btn-sm rounded"
+                           target="_blank"
+                           rel="noreferrer"
+                           href={`${appConfig.apiUrl()}/${getPath()}`}>View fullscreen</a>
+                </div>
             </div>}
         </div>
     );
