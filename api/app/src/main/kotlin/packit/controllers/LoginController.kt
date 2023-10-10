5b31c2cd3e734ba5b4d9463e352e72ddcd1bee8f
Lekan Anifowoshe <anney.lake@gmail.com>
2023-10-10 17:27:38 +0100

    remove unused imports

--- api/app/src/main/kotlin/packit/controllers/LoginController.kt
+++ api/app/src/main/kotlin/packit/controllers/LoginController.kt
@@ -1,7 +1,6 @@
 package packit.controllers
 
 import org.springframework.http.ResponseEntity
-import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 import packit.model.LoginRequest
