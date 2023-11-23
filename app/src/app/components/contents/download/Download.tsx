a7ee3449d46497a9881e711cf609e362a3c03993
anmol thapar <mr.anmolthapar@gmail.com>
2023-11-23 08:37:17 +0000

    chore: remove unessary dependencies

--- app/src/app/components/contents/download/Download.tsx
+++ app/src/app/components/contents/download/Download.tsx
@@ -1,58 +1,55 @@
-import {useSelector} from "react-redux";
-import {FileMetadata, RootState, useAppDispatch} from "../../../../types";
-import React, {useEffect} from "react";
-import {actions} from "../../../store/packets/packetThunks";
-import {Link, useParams} from "react-router-dom";
-import {CloudDownload} from "@mui/icons-material";
+import { useSelector } from "react-redux";
+import { FileMetadata, RootState, useAppDispatch } from "../../../../types";
+import React, { useEffect } from "react";
+import { actions } from "../../../store/packets/packetThunks";
+import { Link, useParams } from "react-router-dom";
 import appConfig from "../../../../config/appConfig";
-import {bytesToSize} from "../../../../helpers";
-import {PacketHeader} from "../packets";
+import { bytesToSize } from "../../../../helpers";
+import { PacketHeader } from "../packets";
+import { DownloadCloud } from "lucide-react";
 
-export default function Download()
-{
-    const {packet} = useSelector((state: RootState) => state.packets);
+export default function Download() {
+  const { packet } = useSelector((state: RootState) => state.packets);
 
-    const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch();
 
-    const {packetId} = useParams();
+  const { packetId } = useParams();
 
-    useEffect(() => {
-        if (packetId) {
-            dispatch(actions.fetchPacketById(packetId));
-        }
-    }, [packetId]);
-
-    const download = (file: FileMetadata) => {
-        return `${appConfig.apiUrl()}/packets/file/${file.hash}?filename=${file.path}`;
-    };
-
-    if (Object.keys(packet).length === 0) {
-        return (<div>Loading...</div>);
+  useEffect(() => {
+    if (packetId) {
+      dispatch(actions.fetchPacketById(packetId));
     }
+  }, [packetId]);
 
-    return (
-        <div className="content packet-details">
-            <PacketHeader packet={packet}/>
-            <ul className="list-unstyled">
-                {packet.files.map((data, key) =>
-                    (
-                        <li key={key} className="pb-2">
-                            <div className="card custom-card">
-                                <div className="card-header">
-                                    Download {data.path}
-                                </div>
-                                <div className="card-body">
-                                    <Link className="card-text" to={download(data)}>
-                                        <span className="p-2">{data.path}</span>
-                                        <span className="sidebar-icon"><CloudDownload/></span>
-                                    </Link>
-                                    <span className="small p-2 text-muted">({bytesToSize(data.size)})</span>
-                                </div>
-                            </div>
-                        </li>
-                    )
-                )}
-            </ul>
-        </div>
-    );
+  const download = (file: FileMetadata) => {
+    return `${appConfig.apiUrl()}/packets/file/${file.hash}?filename=${file.path}`;
+  };
+
+  if (Object.keys(packet).length === 0) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="content packet-details">
+      <PacketHeader packet={packet} />
+      <ul className="list-unstyled">
+        {packet.files.map((data, key) => (
+          <li key={key} className="pb-2">
+            <div className="card custom-card">
+              <div className="card-header">Download {data.path}</div>
+              <div className="card-body">
+                <Link className="card-text" to={download(data)}>
+                  <span className="p-2">{data.path}</span>
+                  <span className="sidebar-icon">
+                    <DownloadCloud />
+                  </span>
+                </Link>
+                <span className="small p-2 text-muted">({bytesToSize(data.size)})</span>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
--- app/src/app/components/contents/explorer/Table.tsx
+++ app/src/app/components/contents/explorer/Table.tsx
@@ -1,73 +1,81 @@
-import React, {useState} from "react";
-import {Header, Packet, PacketTableProps} from "../../../../types";
-import {FaSort} from "react-icons/fa";
-import {Link} from "react-router-dom";
+import React, { useState } from "react";
+import { Header, Packet, PacketTableProps } from "../../../../types";
+
+import { Link } from "react-router-dom";
+import { SortAsc } from "lucide-react";
 
 const headers: Header[] = [
-    {label: "Name", accessor: "displayName", sortable: true},
-    {label: "Version", accessor: "id", sortable: false},
-    {label: "Status", accessor: "published", sortable: true},
-    {label: "Parameters", accessor: "parameters", sortable: false}
+  { label: "Name", accessor: "displayName", sortable: true },
+  { label: "Version", accessor: "id", sortable: false },
+  { label: "Status", accessor: "published", sortable: true },
+  { label: "Parameters", accessor: "parameters", sortable: false }
 ];
 
-export default function Table({data}: PacketTableProps) {
-    const [ascending, setAscending] = useState(true);
-    const [sortColumn, setSortColumn] = useState("");
+export default function Table({ data }: PacketTableProps) {
+  const [ascending, setAscending] = useState(true);
+  const [sortColumn, setSortColumn] = useState("");
 
-    const sortPackets = (accessor: string) => {
-        const newAscending = (accessor === sortColumn) ? !ascending : true;
-        setAscending(newAscending);
-        setSortColumn(accessor);
+  const sortPackets = (accessor: string) => {
+    const newAscending = accessor === sortColumn ? !ascending : true;
+    setAscending(newAscending);
+    setSortColumn(accessor);
 
-        data.sort((a, b) => (`${a[accessor as keyof Packet]}`
-            .localeCompare(`${b[accessor as keyof Packet]}`)) * (newAscending ? 1 : -1));
-    };
-
-    return (
-        <table data-testid="table" className="table table-hover table-bordered table-sm">
-            <thead>
-            <tr>
-                {headers.map(({label, accessor, sortable}) => (
-                    <th key={accessor}>
-                        <span className="m-4"
-                              onClick={() => sortable ? sortPackets(accessor) : null}>
-                            {label}
-                            {sortable ? <span className="px-2 icon-sort"><FaSort/></span> : ""}
-                        </span>
-                    </th>
-                ))}
-            </tr>
-            </thead>
-            <tbody>
-            {data.map((packet, key) => (
-                <tr key={`row-${key}`}>
-                    <td>
-                        <span>
-                            <Link to={`/packets/${packet.id}/packet`}>
-                                {packet.displayName ? packet.displayName : packet.name}
-                            </Link>
-                        </span>
-                    </td>
-                    <td>
-                        <span>{packet.id}</span>
-                    </td>
-                    <td>
-                        <span className={`badge ${packet.published ? "badge-published" : "badge-internal"}`}>
-                                {packet.published ? "published" : "internal"}
-                        </span>
-                    </td>
-                    <td>
-                        <span>
-                            <ul className="list-unstyled ">
-                                {Object.entries(packet.parameters).map(([key, value]) => (
-                                    <li className="justify-content-evenly" key={`col-${key}`}>{key}={value}</li>
-                                ))}
-                            </ul>
-                        </span>
-                    </td>
-                </tr>
-            ))}
-            </tbody>
-        </table>
+    data.sort(
+      (a, b) =>
+        `${a[accessor as keyof Packet]}`.localeCompare(`${b[accessor as keyof Packet]}`) * (newAscending ? 1 : -1)
     );
+  };
+
+  return (
+    <table data-testid="table" className="table table-hover table-bordered table-sm">
+      <thead>
+        <tr>
+          {headers.map(({ label, accessor, sortable }) => (
+            <th key={accessor}>
+              <span className="m-4" onClick={() => (sortable ? sortPackets(accessor) : null)}>
+                {label}
+                {sortable ? (
+                  <span className="px-2 icon-sort">
+                    <SortAsc />
+                  </span>
+                ) : (
+                  ""
+                )}
+              </span>
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((packet, key) => (
+          <tr key={`row-${key}`}>
+            <td>
+              <span>
+                <Link to={`${packet.id}`}>{packet.displayName ? packet.displayName : packet.name}</Link>
+              </span>
+            </td>
+            <td>
+              <span>{packet.id}</span>
+            </td>
+            <td>
+              <span className={`badge ${packet.published ? "badge-published" : "badge-internal"}`}>
+                {packet.published ? "published" : "internal"}
+              </span>
+            </td>
+            <td>
+              <span>
+                <ul className="list-unstyled ">
+                  {Object.entries(packet.parameters).map(([key, value]) => (
+                    <li className="justify-content-evenly" key={`col-${key}`}>
+                      {key}={value}
+                    </li>
+                  ))}
+                </ul>
+              </span>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
