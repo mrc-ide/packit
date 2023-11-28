267eced7cbf0fed62cfd917609d54712e1372993
EmmaLRussell <EmmaRussellKilburn@gmail.com>
2023-11-28 16:53:27 +0000

    exception handler tests

--- api/app/src/test/kotlin/packit/integration/exceptions/PackitExceptionHandlerTest.kt
+++ api/app/src/test/kotlin/packit/integration/exceptions/PackitExceptionHandlerTest.kt
@@ -1,8 +1,11 @@
-package packit.integration.exceptionss
+package packit.integration.exceptions
 
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.web.client.exchange
 import org.springframework.http.*
+import org.springframework.http.client.SimpleClientHttpRequestFactory
 import packit.integration.IntegrationTest
 import packit.integration.WithAuthenticatedUser
 import kotlin.test.assertEquals
@@ -10,6 +13,16 @@ import kotlin.test.assertEquals
 @WithAuthenticatedUser
 class PackitExceptionHandlerTest : IntegrationTest()
 {
+    @BeforeEach
+    fun setup() {
+        // Default test rest template settings throw when examine
+        // status of 401 responses, using alternative factory fixes this
+        // https://stackoverflow.com/questions/16748969
+        val factory = SimpleClientHttpRequestFactory()
+        factory.setOutputStreaming(false)
+        restTemplate.restTemplate.requestFactory = factory
+    }
+
     @Test
     fun `throws exception when client error occurred`()
     {
@@ -35,5 +48,21 @@ class PackitExceptionHandlerTest : IntegrationTest()
         )
 
         assertEquals(result.statusCode, HttpStatus.BAD_REQUEST)
+    }
+
+    @Test
+    fun `throws unauthorized exception when token is invalid`()
+    {
+        val headers = HttpHeaders()
+        val entity = HttpEntity(mapOf("githubtoken" to "xyz"), headers)
+
+        // NB this test will hit the real github api
+        val result: ResponseEntity<String> = restTemplate.exchange(
+            "/auth/login/github",
+            HttpMethod.POST,
+            entity
+        )
+
+        assertEquals(result.statusCode, HttpStatus.UNAUTHORIZED)
     }
 }
---
+++ api/app/src/test/kotlin/packit/unit/exceptions/PackitExceptionHandlerTest.kt
@@ -0,0 +1,17 @@
+package packit.unit.exceptions
+
+import org.junit.jupiter.api.Test
+import packit.exceptions.PackitAuthenticationException
+import packit.exceptions.PackitExceptionHandler
+import kotlin.test.assertEquals
+
+class PackitExceptionHandlerTest {
+    @Test
+    fun `returns error details for PackitAuthenticationException`()
+    {
+        val exception = PackitAuthenticationException("githubTokenInsufficientPermissions")
+        val sut = PackitExceptionHandler()
+        val result = sut.errorDetailForPackitAuthenticationException(exception)
+        assertEquals(result.detail, "The supplied GitHub token is invalid or does not have sufficient permissions to check user credentials.")
+    }
+}
