ee321a7bd8e076a7371d2d72ff95363bf19fa211
EmmaLRussell <EmmaRussellKilburn@gmail.com>
2023-12-05 14:54:42 +0000

    regenerate test token

--- app/src/tests/integration/IntegrationTest.ts
+++ app/src/tests/integration/IntegrationTest.ts
@@ -1,5 +1,5 @@
 // Long-lived JWT token for test backend configuration which we can use to authenticate in integration tests
 export const testUser = {
     // eslint-disable-next-line max-len
-    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJwYWNraXQiLCJpc3MiOiJwYWNraXQtYXBpIiwiZW1haWwiOiJudWxsIiwibmFtZSI6IkVtbWEgUnVzc2VsbCIsImRhdGV0aW1lIjoxNzAxNzY5Njk0LCJhdSI6WyJVU0VSIl0sImV4cCI6MjMzMjQ4OTY5NH0.916rRWeyyk4GAOwAOu0EgrgYd271GDib4pRbWAe_Gyk"
+    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJwYWNraXQiLCJpc3MiOiJwYWNraXQtYXBpIiwidXNlck5hbWUiOiJFbW1hTFJ1c3NlbGwiLCJkaXNwbGF5TmFtZSI6IkVtbWEgUnVzc2VsbCIsImRhdGV0aW1lIjoxNzAxNzg3OTA5LCJhdSI6WyJbVVNFUl0iXSwiZXhwIjoyMzMyNTA3OTA5fQ.v77D6zigM3GOapJ0q1SQy1nTAaXQNokPiR4AiRwe79Q"
 };
