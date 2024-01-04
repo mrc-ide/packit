62f27eb08af76ef26fb5c7ab0de5ae9f0a8832e2
EmmaLRussell <EmmaRussellKilburn@gmail.com>
2024-01-04 13:41:46 +0000

    link to object url

--- app/src/app/components/contents/packets/PacketReports.tsx
+++ app/src/app/components/contents/packets/PacketReports.tsx
@@ -3,6 +3,8 @@ import { NavLink } from "react-router-dom";
 import appConfig from "../../../../config/appConfig";
 import { PacketMetadata } from "../../../../types";
 import { getHtmlFilePath } from "./utils/getHtmlFilePath";
+import {getFileObjectUrl} from "../../../../lib/download";
+import {useEffect, useState} from "react";
 
 interface PacketReportsProps {
   packet: PacketMetadata | undefined;
@@ -11,12 +13,24 @@ interface PacketReportsProps {
 export const PacketReports = ({ packet }: PacketReportsProps) => {
   const htmlFilePath = packet ? getHtmlFilePath(packet) : null;
 
+
+  const [htmlFileObjectUrl, setHtmlFileObjectUrl] = useState(undefined as string | undefined);
+  const getHtmlFileObjectUrl = async () => {
+    if (htmlFilePath) {
+      setHtmlFileObjectUrl(await getFileObjectUrl(`${appConfig.apiUrl()}/${htmlFilePath}`, ""));
+    }
+  };
+
+  useEffect(() => {
+    getHtmlFileObjectUrl();
+  });
+
   return (
     <div>
       <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">Reports</h4>
       {htmlFilePath ? (
         <div className="h-screen">
-          <iframe className="w-full h-2/3" src={`${appConfig.apiUrl()}/${htmlFilePath}`}></iframe>
+          <iframe className="w-full h-2/3" src={htmlFileObjectUrl}></iframe>
           <div className="py-2 flex justify-end">
             <NavLink
               className="text-blue-500 flex items-center gap-1
