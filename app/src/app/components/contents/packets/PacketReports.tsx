0c73a01ab746cf64328039cc656553a4998e7671
EmmaLRussell <EmmaRussellKilburn@gmail.com>
2024-01-09 14:30:16 +0000

    cancel dark background for iframes and put border around report

--- app/src/app/components/contents/packets/PacketReports.tsx
+++ app/src/app/components/contents/packets/PacketReports.tsx
@@ -28,7 +28,7 @@ export const PacketReports = ({ packet }: PacketReportsProps) => {
       <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">Reports</h4>
       {htmlFileObjectUrl ? (
         <div className="h-screen">
-          <iframe className="w-full h-2/3" data-testid="report-iframe" src={htmlFileObjectUrl}></iframe>
+          <iframe className="w-full h-2/3 border" data-testid="report-iframe" src={htmlFileObjectUrl}></iframe>
           <div className="py-2 flex justify-end">
             <a
               className="text-blue-500 flex items-center gap-1
