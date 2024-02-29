7e0bfc68ea7446efab02c1d4e6a5b8f777b9e8d4
EmmaLRussell <EmmaRussellKilburn@gmail.com>
2024-02-29 09:21:19 +0000

    test get 401 with bad token"

--- api/app/src/test/kotlin/packit/integration/controllers/LoginControllerTest.kt
+++ api/app/src/test/kotlin/packit/integration/controllers/LoginControllerTest.kt
@@ -3,11 +3,10 @@ package packit.integration.controllers
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.springframework.http.HttpEntity
-import org.springframework.http.HttpHeaders
-import org.springframework.http.MediaType
+import org.springframework.http.*
 import packit.integration.IntegrationTest
 import packit.model.LoginWithToken
+import kotlin.test.assertEquals
 
 class LoginControllerTest : IntegrationTest()
 {
@@ -24,18 +23,29 @@ class LoginControllerTest : IntegrationTest()
     {
         val token = env.getProperty("GITHUB_ACCESS_TOKEN")!!
         assertThat(token.count()).isEqualTo(40) // sanity check access token correctly saved in environment
+        val result = getLoginResponse(token)
+
+        assertSuccess(result)
+        val packitToken = ObjectMapper().readTree(result.body).get("token").asText()
+        assertThat(packitToken.count()).isGreaterThan(0)
+    }
+
+    @Test
+    fun `can receive 401 response when login to API with invalid token`()
+    {
+        val result = getLoginResponse("badtoken")
+        assertEquals(result.statusCode, HttpStatus.UNAUTHORIZED)
+    }
+
+    private fun getLoginResponse(token: String): ResponseEntity<String>
+    {
         val postBody = LoginWithToken(token)
 
         val headers = HttpHeaders()
         headers.contentType = MediaType.APPLICATION_JSON
 
-        val objectMapper = ObjectMapper()
-        val jsonString = objectMapper.writeValueAsString(postBody)
+        val jsonString = ObjectMapper().writeValueAsString(postBody)
         val postEntity = HttpEntity(jsonString, headers)
-        val result = restTemplate.postForEntity<String>("/auth/login/api", postEntity, String::class.java)
-
-        assertSuccess(result)
-        val packitToken = objectMapper.readTree(result.body).get("token").asText()
-        assertThat(packitToken.count()).isGreaterThan(0)
+        return restTemplate.postForEntity<String>("/auth/login/api", postEntity, String::class.java)
     }
 }
