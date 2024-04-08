8e0b776ddbd49852bbeb8e57768fb8524b46f4aa
anmol thapar <mr.anmolthapar@gmail.com>
2024-04-08 14:07:10 +0100

    tests: unit test fetching user from db

--- api/app/src/test/kotlin/packit/unit/service/BasicUserDetailsServiceTest.kt
+++ api/app/src/test/kotlin/packit/unit/service/BasicUserDetailsServiceTest.kt
@@ -1,33 +1,49 @@
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.assertThrows
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
 import org.springframework.security.core.userdetails.UserDetails
 import packit.exceptions.PackitException
+import packit.model.User
+import packit.repository.UserRepository
 import packit.service.BasicUserDetailsService
 import kotlin.test.Test
 
 class BasicUserDetailsServiceTest
 {
-
-    private val service = BasicUserDetailsService()
+    private val mockUserRepository = mock<UserRepository>()
+    private val service = BasicUserDetailsService(mockUserRepository)
+    private val mockUser = User(
+        "id",
+        "username",
+        "email",
+        "displayName",
+        listOf(),
+        false,
+        "password",
+        "userSource",
+        "lastLoggedIn"
+    )
 
     @Test
     fun `loadUserByUsername returns correct UserDetails for valid username`()
     {
-        val username = BasicUserDetailsService.USERNAME
+        whenever(mockUserRepository.findByUsername(mockUser.username)).thenReturn(
+            mockUser
+        )
+        val userDetails: UserDetails = service.loadUserByUsername(mockUser.username)
 
-        val userDetails: UserDetails = service.loadUserByUsername(username)
-
-        assertEquals(username, userDetails.username)
-        assertEquals(BasicUserDetailsService.PASSWORD, userDetails.password)
+        assertEquals(mockUser.username, userDetails.username)
+        assertEquals(mockUser.password, userDetails.password)
         assertTrue(userDetails.isEnabled)
     }
 
     @Test
-    fun `loadUserByUsername throws PackitException for invalid username`()
+    fun `loadUserByUsername throws PacketException if repository does not return user`()
     {
         val invalidUsername = "invalid@example.com"
-
+        whenever(mockUserRepository.findByUsername(invalidUsername)).thenReturn(null)
         assertThrows<PackitException> {
             service.loadUserByUsername(invalidUsername)
         }
