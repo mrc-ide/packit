27d0103d3c1302822a0fc66122286e07ceffe74b
anmol thapar <mr.anmolthapar@gmail.com>
2024-05-07 11:19:48 +0100

    Refactor RoleControllerTest***

--- api/app/src/test/kotlin/packit/integration/controllers/RoleControllerTest.kt
+++ api/app/src/test/kotlin/packit/integration/controllers/RoleControllerTest.kt
@@ -1,9 +1,6 @@
 package packit.integration.controllers
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import kotlin.test.assertEquals
-import kotlin.test.assertNotNull
-import kotlin.test.assertNull
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpMethod
@@ -18,6 +15,9 @@ import packit.model.dto.UpdateRolePermission
 import packit.model.toDto
 import packit.repository.PermissionRepository
 import packit.repository.RoleRepository
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
 
 @Sql("/delete-test-users.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 class RoleControllerTest : IntegrationTest()
@@ -161,8 +161,42 @@ class RoleControllerTest : IntegrationTest()
     }
 
     @Test
-    @WithAuthenticatedUser(authorities = ["user.manage"])
+    @WithAuthenticatedUser(authorities = ["none"])
+    fun `user without user manage permission cannot remove roles from permissions`()
+    {
+        roleRepository.save(Role(name = "testRole"))
+
+        val result =
+            restTemplate.exchange(
+                "/role/remove-permissions/testRole",
+                HttpMethod.PUT,
+                getTokenizedHttpEntity(data = updateRolePermission),
+                String::class.java
+            )
+
+        assertEquals(result.statusCode, HttpStatus.UNAUTHORIZED)
+    }
+
+    @Test
+    @WithAuthenticatedUser(authorities = ["none"])
     fun `user without user manage permission cannot add roles to permissions`()
+    {
+        roleRepository.save(Role(name = "testRole"))
+
+        val result =
+            restTemplate.exchange(
+                "/role/add-permissions/testRole",
+                HttpMethod.PUT,
+                getTokenizedHttpEntity(data = updateRolePermission),
+                String::class.java
+            )
+
+        assertEquals(result.statusCode, HttpStatus.UNAUTHORIZED)
+    }
+
+    @Test
+    @WithAuthenticatedUser(authorities = ["user.manage"])
+    fun `user with manage authority can read role names`()
     {
         roleRepository.save(Role(name = "testRole"))
 
@@ -179,7 +213,7 @@ class RoleControllerTest : IntegrationTest()
         assertEquals(ObjectMapper().writeValueAsString(listOf("ADMIN", "testRole")), result.body)
     }
 
-    
+
     @Test
     @WithAuthenticatedUser(authorities = ["user.manage"])
     fun `users can get roles with relationships`()
@@ -254,20 +288,4 @@ class RoleControllerTest : IntegrationTest()
         assertEquals(ObjectMapper().writeValueAsString(roleDto), result.body)
     }
 
-    @Test
-    @WithAuthenticatedUser(authorities = ["none"])
-    fun `user without user manage permission cannot remove roles from permissions`()
-    {
-        roleRepository.save(Role(name = "testRole"))
-
-        val result =
-            restTemplate.exchange(
-                "/role/remove-permissions/testRole",
-                HttpMethod.PUT,
-                getTokenizedHttpEntity(data = updateRolePermission),
-                String::class.java
-            )
-
-        assertEquals(result.statusCode, HttpStatus.UNAUTHORIZED)
-    }
 }
