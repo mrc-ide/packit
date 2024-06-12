1542d4430fe026c70172db5f55cd558c7a048077
anmol thapar <mr.anmolthapar@gmail.com>
2024-06-12 09:57:31 +0100

    feat: report string fix

--- app/src/app/components/contents/explorer/hooks/useGetPacketGroupSummary.ts
+++ app/src/app/components/contents/explorer/hooks/useGetPacketGroupSummary.ts
@@ -2,7 +2,6 @@ import useSWR from "swr";
 import appConfig from "../../../../../config/appConfig";
 import { fetcher } from "../../../../../lib/fetch";
 import { PageablePacketGroupSummary } from "../../../../../types";
-import { ApiError } from "../../../../../lib/errors";
 
 export const useGetPacketGroupSummary = (pageNumber: number, pageSize: number, filterByName: string) => {
   const { data, isLoading, error } = useSWR<PageablePacketGroupSummary>(
