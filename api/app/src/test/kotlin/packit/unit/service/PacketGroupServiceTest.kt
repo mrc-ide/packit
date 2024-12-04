2bb9650b1b4c109a97bc6fc668f1c56492636d78
David Mears <clarinet.david@gmail.com>
2024-12-04 13:48:46 +0000

    Updates to api tests

--- api/app/src/test/kotlin/packit/unit/service/PacketGroupServiceTest.kt
+++ api/app/src/test/kotlin/packit/unit/service/PacketGroupServiceTest.kt
@@ -20,7 +20,7 @@ class PacketGroupServiceTest
         val filterName = "test"
         val packetGroups = listOf(PacketGroup("test1"), PacketGroup("test2"))
         whenever(
-            packetGroupRepository.findAllByNameContaining(
+            packetGroupRepository.findPacketGroupsBySearchString(
                 eq(filterName), any<Sort>()
             )
         ).thenReturn(packetGroups)
@@ -28,7 +28,7 @@ class PacketGroupServiceTest
         val result = sut.getPacketGroups(pageablePayload, filterName)
 
         assertEquals(packetGroups, result.content)
-        verify(packetGroupRepository).findAllByNameContaining(
+        verify(packetGroupRepository).findPacketGroupsBySearchString(
             filterName,
             Sort.by("name")
         )
