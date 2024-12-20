0c8e9e4d39c51add0971acbabe9621b4fe32f01a
David Mears <clarinet.david@gmail.com>
2024-12-20 18:40:44 +0000

    Remove unused function - now moved to using outpack_server

--- api/app/src/main/kotlin/packit/repository/PacketRepository.kt
+++ api/app/src/main/kotlin/packit/repository/PacketRepository.kt
@@ -18,32 +18,6 @@ interface PacketRepository : JpaRepository<Packet, String>
     @PostFilter("@authz.canReadPacket(#root, filterObject.id, filterObject.name)")
     fun findByName(name: String, sort: Sort): List<Packet>
 
-    @PostFilter("@authz.canReadPacketGroup(#root, filterObject.name)")
-    @Query(
-        value = """
-                SELECT 
-                    name as name, 
-                    start_time as latestTime, 
-                    id AS latestId,
-                    id_count as packetCount,
-                    display_name as latestDisplayName
-                FROM ( 
-                    SELECT 
-                        name,
-                        start_time, 
-                        id,
-                        display_name,
-                        ROW_NUMBER() OVER (PARTITION BY name ORDER BY start_time DESC) AS row_num, 
-                        COUNT(id) OVER (PARTITION BY name) AS id_count 
-                    FROM packet 
-                ) as RankedData
-                WHERE row_num = 1 AND (name ILIKE %?1% OR display_name ILIKE %?1%)
-                ORDER BY start_time DESC
-         """,
-        nativeQuery = true
-    )
-    fun getFilteredPacketGroupSummaries(filter: String): List<PacketGroupSummary>
-
     @PostFilter("@authz.canReadPacket(#root, filterObject.id, filterObject.name)")
     fun findAllByNameContainingAndIdContaining(name: String, id: String, sort: Sort): List<Packet>
 }
