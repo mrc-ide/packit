1334a72827bf82dd6303eb1cf6d7bf848564f0bf
David Mears <clarinet.david@gmail.com>
2024-12-20 13:49:42 +0000

    Remove a migration (adding display name column on packet group) that has not been run in any environment

--- api/app/src/main/resources/db/migration/V20241206__add_latest_display_name_column_to_packet_group.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE "packet_group"
-    ADD COLUMN "latest_display_name" text DEFAULT '';
-
-ALTER TABLE "packet_group"
-    ALTER COLUMN "latest_display_name" SET NOT NULL;
-
-ALTER TABLE "packet_group"
-    ALTER COLUMN "latest_display_name" DROP DEFAULT;
