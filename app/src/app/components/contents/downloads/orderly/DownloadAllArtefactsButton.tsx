efc0ddd636f97b1ce7faf6343d43fd6d0af5d274
David Mears <clarinet.david@gmail.com>
2025-01-21 17:33:55 +0000

    Add files forgotten from prev commit

---
+++ app/src/app/components/contents/downloads/orderly/DownloadAllArtefactsButton.tsx
@@ -0,0 +1,9 @@
+import { Button } from "../../../Base/Button";
+
+export const DownloadAllArtefactsButton = () => {
+  return (
+    <Button variant="ghost" className="h-fit w-fit">
+      Download all artefacts as .zip
+    </Button>
+  );
+};
---
+++ app/src/app/components/contents/downloads/orderly/DownloadAllOtherFilesButton.tsx
@@ -0,0 +1,9 @@
+import { Button } from "../../../Base/Button";
+
+export const DownloadAllOtherFilesButton = () => {
+  return (
+    <Button variant="ghost" className="h-fit w-fit">
+      Download as .zip
+    </Button>
+  );
+};
