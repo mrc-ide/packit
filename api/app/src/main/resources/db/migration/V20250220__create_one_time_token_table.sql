2b032b84c97e3be592cc1879da654d16de108a6e
David Mears <clarinet.david@gmail.com>
2025-02-20 16:26:59 +0000

    remove new migration

--- api/app/src/main/resources/db/migration/V20250220__create_one_time_token_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS "one_time_token"
-(
-    "id"            UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    "packet_id"     TEXT NOT NULL,
-    "files"         TEXT[] NOT NULL,
-    "expires"       TIMESTAMP NOT NULL,
-    FOREIGN KEY ("user_id") REFERENCES "user" ("id") ON DELETE CASCADE,
-    FOREIGN KEY ("packet_id") REFERENCES "packet" ("id") ON DELETE CASCADE
-);
