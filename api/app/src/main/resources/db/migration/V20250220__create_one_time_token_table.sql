e6a8bcf7f3142b79b00318f6a249305b44fb4c8b
David Mears <clarinet.david@gmail.com>
2025-02-21 13:22:30 +0000

    Remove user_id column from one_time_token table
    We already check the user permissions when the token is created.
    We don't need to check that the token belongs to the user.

--- api/app/src/main/resources/db/migration/V20250220__create_one_time_token_table.sql
+++ api/app/src/main/resources/db/migration/V20250220__create_one_time_token_table.sql
@@ -1,10 +1,8 @@
 CREATE TABLE IF NOT EXISTS "one_time_token"
 (
-    "id"        UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    "packet_id" TEXT      NOT NULL,
-    "user_id"   UUID      NOT NULL,
-    "files"     TEXT[]    NOT NULL,
-    "expires"   TIMESTAMP NOT NULL,
-    FOREIGN KEY ("user_id") REFERENCES "user" ("id") ON DELETE CASCADE,
+    "id"         UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    "packet_id"  TEXT      NOT NULL,
+    "file_paths" TEXT[]    NOT NULL,
+    "expires_at" TIMESTAMP NOT NULL,
     FOREIGN KEY ("packet_id") REFERENCES "packet" ("id") ON DELETE CASCADE
 );
