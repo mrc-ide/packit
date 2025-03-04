bb4e79c15e7331dc868f8650e72571fe9b43c756
David Mears <clarinet.david@gmail.com>
2025-03-04 14:00:00 +0000

    small fixes

--- app/src/app/components/contents/home/Home.tsx
+++ app/src/app/components/contents/home/Home.tsx
@@ -20,13 +20,15 @@ export const Home = () => {
     <div className="flex justify-center">
       <div className="h-full flex flex-1 flex-col space-y-6 p-8 max-w-5xl">
         {pinnedPackets && pinnedPackets.length > 0 && (
-          <>
-            <h2 className="text-lg font-bold tracking-tight">Pinned reports</h2>
-            <div className="flex gap-3 flex-wrap me-[-1rem]">
-              {pinnedPackets?.map((packet) => <Pin key={packet.id} packet={packet} />)}
+          <div className="space-y-3">
+            <h2 className="text-lg font-bold tracking-tight mb-6">Pinned reports</h2>
+            <div className="overflow-x-auto pb-3">
+              <div className="flex gap-3">
+                {pinnedPackets?.map((packet) => <Pin key={packet.id} packet={packet} />)}
+              </div>
             </div>
             <Separator />
-          </>
+          </div>
         )}
         <h2 className="text-lg font-bold tracking-tight">Find a report</h2>
         <div className="space-y-4 flex flex-col">
--- app/src/app/components/contents/home/Pin.tsx
+++ app/src/app/components/contents/home/Pin.tsx
@@ -14,7 +14,7 @@ export const Pin = ({ packet }: PinProps) => {
   const allArtefactsFiles = allArtefactsFilesForPacket(packet);
 
   return (
-    <li className="p-4 flex flex-col border rounded shadow min-w-[32%]">
+    <li className="p-3.5 flex flex-col border rounded shadow">
       <div className="flex gap-5 justify-between">
         <Link to={`/${packet.name}/${packet.id}`} className="hover:underline decoration-blue-500 min-w-[50%] w-fit">
           <h3 className="scroll-m-20 text-lg font-semibold tracking-tight text-blue-500">
@@ -23,20 +23,20 @@ export const Pin = ({ packet }: PinProps) => {
         </Link>
         <PinIcon size={18} className="opacity-50"></PinIcon>
       </div>
-      <div className="flex flex-wrap gap-y-2 space-x-3 items-center mt-2 text-xs">
+      <div className="flex gap-x-3 items-center mt-2 text-xs">
         {allArtefactsFiles && (
           <ZipDownloadButton
             packetId={packet.id}
             files={allArtefactsFiles}
             zipName={`${packet.name}_artefacts_${packet.id}.zip`}
             variant="link"
-            buttonText={allArtefactsFiles.length == 0 ? "No artefacts to download" : "Download artefacts"}
+            buttonText={allArtefactsFiles.length == 0 ? "No artefacts" : "Download artefacts"}
             className="text-blue-500 p-0 text-xs"
-            containerClassName="!gap-0"
+            containerClassName="!gap-0 !items-start"
             disabled={allArtefactsFiles.length == 0}
           />
         )}
-        <div className="text-muted-foreground">
+        <div className="text-muted-foreground whitespace-nowrap">
           Ran {value} {unit} ago
         </div>
       </div>
