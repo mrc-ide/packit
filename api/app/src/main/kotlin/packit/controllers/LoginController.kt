fbea7114ab91b2a86680c71ea8ac77576ec7b4ed
Emma <EmmaRussellKilburn@gmail.com>
2025-04-02 15:58:06 +0100

    fix compile

--- api/app/src/main/kotlin/packit/controllers/LoginController.kt
+++ api/app/src/main/kotlin/packit/controllers/LoginController.kt
@@ -112,7 +112,7 @@ class LoginController(
         val authConfig = mapOf(
             "enableGithubLogin" to config.authEnableGithubLogin,
             "enableBasicLogin" to config.authEnableBasicLogin,
-            "enablePreAuthLogin" to config.enablePreAuthLogin,
+            "enablePreAuthLogin" to config.authEnablePreAuthLogin,
             "enableAuth" to config.authEnabled
         )
         return ResponseEntity.ok(authConfig)
