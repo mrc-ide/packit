d3971eb1d23d88dcc4956bea534c9681a781fd9b
David Mears <clarinet.david@gmail.com>
2025-04-11 12:27:50 +0100

    Remove unused UUID generation method
    The API layer is handling the UUID generation.

--- api/app/src/main/resources/db/migration/V20250410__create_one_time_token_table.sql
+++ api/app/src/main/resources/db/migration/V20250410__create_one_time_token_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS "one_time_token"
 (
-    "id"         UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    "id"         UUID PRIMARY KEY,
     "packet_id"  TEXT                     NOT NULL,
     "file_paths" TEXT[]                   NOT NULL,
     "expires_at" TIMESTAMP WITH TIME ZONE NOT NULL,
