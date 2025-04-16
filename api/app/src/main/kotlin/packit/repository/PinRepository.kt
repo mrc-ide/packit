92404712919a84afa6b320db44bb708babc985e5
David Mears <clarinet.david@gmail.com>
2025-04-16 14:58:38 +0100

    Tighten util import

--- api/app/src/main/kotlin/packit/repository/PinRepository.kt
+++ api/app/src/main/kotlin/packit/repository/PinRepository.kt
@@ -2,6 +2,6 @@ package packit.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
 import packit.model.Pin
-import java.util.*
+import java.util.UUID
 
 interface PinRepository : JpaRepository<Pin, UUID>
