c61c04e0ee8cb15b540491a9af17f5e128e8be8e
David Mears <clarinet.david@gmail.com>
2025-04-23 13:44:33 +0100

    Add uniqueness, non-null constraints on migration creating pin table

--- api/app/src/main/resources/db/migration/V20250303__create_pin_table.sql
+++ api/app/src/main/resources/db/migration/V20250303__create_pin_table.sql
@@ -3,6 +3,6 @@ DROP TABLE IF EXISTS "pinned_packet_global";
 CREATE TABLE IF NOT EXISTS "pin"
 (
     "id"        UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    "packet_id" TEXT,
+    "packet_id" TEXT UNIQUE NOT NULL,
     FOREIGN KEY ("packet_id") REFERENCES "packet" ("id") ON DELETE CASCADE
 );
