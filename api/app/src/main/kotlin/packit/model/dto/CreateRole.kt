bc71977f0045d59c6c23e5dd9b2e60aa607071e6
anmol thapar <mr.anmolthapar@gmail.com>
2025-04-25 10:08:07 +0100

    fix: update regex pattern for role name validation to allow spaces between words

--- api/app/src/main/kotlin/packit/model/dto/CreateRole.kt
+++ api/app/src/main/kotlin/packit/model/dto/CreateRole.kt
@@ -6,7 +6,7 @@ import org.jetbrains.annotations.NotNull
 data class CreateRole(
     @field:NotNull
     @field:Pattern(
-        regexp = "^[a-zA-Z0-9]+(\\s[a-zA-Z0-9]+)*\$",
+        regexp = "^[a-zA-Z0-9]+( [a-zA-Z0-9]+)*$",
         message = "Name must only contain alphanumeric characters and cannot have leading or trailing spaces"
     )
     val name: String,
