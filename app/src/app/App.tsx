ea9385969ff8ed0bb0db0ec1197d9e144cdfed98
David Mears <clarinet.david@gmail.com>
2025-04-29 10:29:19 +0100

    Set page title dynamically based on branding configuration

--- app/src/app/App.tsx
+++ app/src/app/App.tsx
@@ -1,7 +1,18 @@
 import { Header } from "./components/header";
 import { Main } from "./components/main";
+import { useEffect } from "react";
+import { useGetBranding } from "./components/contents/common/hooks/useGetBranding";
 
 export const App = () => {
+  const { brandConfig } = useGetBranding();
+
+  useEffect(() => {
+    console.log(brandConfig?.brandName)
+    if (brandConfig?.brandName) {
+      document.title = brandConfig?.brandName;
+    }
+  }, [brandConfig?.brandName]);
+
   return (
     <div data-testid="app">
       <Header />
