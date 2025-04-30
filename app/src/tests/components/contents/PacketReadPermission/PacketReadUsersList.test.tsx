4a69adbfd80561743184d08f894c8d837fda4b37
anmol thapar <mr.anmolthapar@gmail.com>
2025-04-30 11:50:36 +0100

    fix: correct text in PacketReadUsersList test and update user type

--- app/src/tests/components/contents/PacketReadPermission/PacketReadUsersList.test.tsx
+++ app/src/tests/components/contents/PacketReadPermission/PacketReadUsersList.test.tsx
@@ -11,7 +11,7 @@ describe("PacketReadUsersList", () => {
 
     render(<PacketReadUsersList users={users} />);
 
-    expect(screen.getByText(/specific users with read access/i)).toBeVisible();
+    expect(screen.getByText(/UserWithRolesspecific users with read access/i)).toBeVisible();
     expect(screen.getByText(/users granted access on an individual basis/i)).toBeVisible();
     users.forEach((user) => {
       expect(screen.getByText(user.username)).toBeVisible();
@@ -19,7 +19,7 @@ describe("PacketReadUsersList", () => {
   });
 
   it("should render none when no users in list", async () => {
-    const users: UserWithRoles[] = [];
+    const users: UserWithPermissions[] = [];
 
     render(<PacketReadUsersList users={users} />);
 
