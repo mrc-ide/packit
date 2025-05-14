89e6c5d418c15dd03108fea086d9b24cfc7d96c2
Emma <EmmaRussellKilburn@gmail.com>
2025-05-14 11:59:10 +0100

    clean up after Login github test

--- api/app/src/test/kotlin/packit/integration/controllers/LoginControllerTest.kt
+++ api/app/src/test/kotlin/packit/integration/controllers/LoginControllerTest.kt
@@ -32,6 +32,31 @@ import kotlin.test.assertEquals
 
 class LoginControllerTestGithub : IntegrationTest()
 {
+    @Autowired
+    private lateinit var userRepository: UserRepository
+
+    @Autowired
+    private lateinit var roleRepository: RoleRepository
+
+    @Autowired
+    private lateinit var tokenDecoder: TokenDecoder
+
+    @AfterEach
+    fun cleanupData()
+    {
+        val githubUsers = userRepository.findAll().filter{ it.userSource == "github" }
+        githubUsers.forEach {
+            val userName = it.username
+            if (userRepository.existsByUsername(userName)) {
+                userRepository.deleteByUsername(userName)
+            }
+
+            if (roleRepository.existsByName(userName)) {
+                roleRepository.deleteByName(userName)
+            }
+        }
+    }
+
     @Test
     fun `can get config`()
     {
