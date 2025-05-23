71c4554e6c5b7d5ebc65b3445e71d0512da3ccce
David Mears <clarinet.david@gmail.com>
2025-05-23 15:38:41 +0100

    Update integration test for logo config controller

--- api/app/src/test/kotlin/packit/integration/controllers/BrandLogoControllerTest.kt
+++ api/app/src/test/kotlin/packit/integration/controllers/BrandLogoControllerTest.kt
@@ -1,12 +1,21 @@
 package packit.integration.controllers
 
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.junit.jupiter.api.Test
 import packit.integration.IntegrationTest
+import kotlin.test.assertEquals
+import packit.model.dto.LogoDto
 
 class BrandLogoControllerTest : IntegrationTest() {
     @Test
     fun `can get config`() {
         val result = restTemplate.getForEntity("/logo/config", String::class.java)
+
+        val body = jacksonObjectMapper().readValue(result.body!!, object: TypeReference<LogoDto>() {})
+        assertEquals(body.altText, "Test the logo alt text")
+        assertEquals(body.filename, "testTheLogoFileName.png")
+        assertEquals(body.linkDestination, "https://www.example.org/test-the-logo-link")
 
         assertSuccess(result)
     }
