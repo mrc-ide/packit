5efa9b50cfd53951afcd664ef691a79414ee576f
Emma <EmmaRussellKilburn@gmail.com>
2025-06-04 11:05:52 +0100

    do not use fetcher

--- app/src/app/components/login/DeviceLogin.tsx
+++ app/src/app/components/login/DeviceLogin.tsx
@@ -1,43 +1,42 @@
 import VerificationInput from "react-verification-input";
 import {Button} from "../Base/Button";
-import {fetcher} from "../../../lib/fetch";
 import appConfig from "../../../config/appConfig";
 import {ApiError} from "../../../lib/errors";
 import {HttpStatus} from "../../../lib/types/HttpStatus";
 import {useState} from "react";
+import {getAuthHeader} from "../../../lib/auth/getAuthHeader";
+import {PacketErrorBody} from "../../../types";
 
 export const DeviceLogin = () => {
 
     const USER_CODE_LENGTH = 9;
 
-    let resultStatus = null;
+    //let resultStatus = null;
     const [userCode, setUserCode] = useState('');
+    const [resultStatus, setResultStatus] = useState<HttpStatus | null>(null);
 
     // TODO: auto focus input
     // TODO: enter key should press button if code is complete
 
+    const handleChange = (newValue: string) => {
+        setUserCode(newValue);
+    };
 
     const handleSubmit = async () => {
-    try {
-        const data: { token: string } = await fetcher({
-            url: `${appConfig.apiUrl()}/deviceAuth/validate`,
-            body: userCode,
+        const url = `${appConfig.apiUrl()}/deviceAuth/validate`;
+        const res = await fetch(url, {
             method: "POST",
-            noAuth: false
-        });
-        // TODO: set result status for sucess and error
-    } catch (error) {
-        console.error(error);
-        if (error instanceof ApiError) {
-            if (error.status === HttpStatus.Forbidden) {
-
+            body: userCode,
+            headers: {
+                "Content-Type": "text.plain",
+                ...getAuthHeader()
             }
-        }
-    }
+        });
+        setResultStatus(res.status as HttpStatus);
+    };
 
     return (
         <>
-
             <div className="space-y-4 text-center mt-8">
                 <h1 className="text-2xl font-semibold tracking-tight">Packit API Login</h1>
                 {resultStatus == HttpStatus.OK && (
@@ -52,9 +51,10 @@ export const DeviceLogin = () => {
                         validChars={'A-Z-'}
                         placeholder={""}
                         autoFocus={true}
+                        onChange={handleChange}
                         classNames={ {container: "mx-auto"} }
                     />
-                    <Button type="submit" onSubmit={handleSubmit} disabled={userCode.length !== USER_CODE_LENGTH}>
+                    <Button onClick={handleSubmit} disabled={userCode.length !== USER_CODE_LENGTH}>
                         Continue
                     </Button>
                     </>
@@ -63,6 +63,7 @@ export const DeviceLogin = () => {
                 {resultStatus == HttpStatus.BadRequest && (
                     <div>Code has expired or is not recognised.</div>
                 )}
+
             </div>
         </>
     );
