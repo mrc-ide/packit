7c9b06611fb9c877efff6b0dbbdcc56cdb4705bd
Emma <EmmaRussellKilburn@gmail.com>
2025-06-05 12:07:53 +0100

    token endpoint

--- api/app/src/main/kotlin/packit/controllers/DeviceAuthController.kt
+++ api/app/src/main/kotlin/packit/controllers/DeviceAuthController.kt
@@ -2,22 +2,31 @@ package packit.controllers
 
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import packit.AppConfig
 import packit.model.dto.DeviceAuthDto
+import packit.model.dto.DeviceAuthFetchToken
+import packit.model.dto.DeviceAuthTokenDto
 import packit.security.oauth2.deviceFlow.DeviceAuthRequest
 import packit.security.profile.UserPrincipal
+import packit.security.provider.JwtIssuer
 import packit.service.DeviceAuthRequestService
+import kotlin.time.Duration.Companion.days
 
 @RestController
 @RequestMapping("/deviceAuth")
 class DeviceAuthController(
     private val appConfig: AppConfig,
-    private val deviceAuthRequestService: DeviceAuthRequestService
+    private val deviceAuthRequestService: DeviceAuthRequestService,
+    private val jwtIssuer: JwtIssuer
 ) {
+
+    private val expiresIn = appConfig.authExpiryDays.days.inWholeSeconds
+
     // TODO: link to spec
     // TODO: note somewhere - keeping this at a different base root from main "auth" just to keep it separate, but open to argument!
     // TODO: note somewhere - seems odd for POST w no body, but that's what spec seems to suggest
@@ -44,5 +53,14 @@ class DeviceAuthController(
         } else {
             ResponseEntity.badRequest().build()
         }
+    }
+
+    @PostMapping("/token")
+    fun fetchToken(@RequestBody @Validated deviceAuthFetchToken: DeviceAuthFetchToken): ResponseEntity<DeviceAuthTokenDto> {
+        // Get request from service by device code
+        // TODO: if grant is wrong, error = unsupported_grant_type
+        val validatingUser = deviceAuthRequestService.useValidatedRequest(deviceAuthFetchToken.deviceCode)
+        val token = jwtIssuer.issue(validatingUser)
+        return ResponseEntity.ok(DeviceAuthTokenDto(token, expiresIn))
     }
 }
---
+++ api/app/src/main/kotlin/packit/exceptions/DeviceAuthTokenException.kt
@@ -0,0 +1,5 @@
+package packit.exceptions
+
+import org.springframework.http.HttpStatus
+
+class DeviceAuthTokenException(val error: String): PackitException(error, HttpStatus.BAD_REQUEST)
