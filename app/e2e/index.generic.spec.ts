2fcb606cea2d473e716ef4780b30fe85a9bca41c
David Mears <clarinet.david@gmail.com>
2025-06-20 11:18:23 +0100

    Update e2e tests, adding an index page demo e2e test for pins

--- app/e2e/index.generic.spec.ts
+++ app/e2e/index.generic.spec.ts
@@ -4,11 +4,9 @@ import {
   createEmptyTestRole,
   getBreadcrumbLocator,
   getPacketGroupIndexLocator,
-  getPinsLocator,
   getReadableIdString,
   navigateToFirstPacketGroup,
   navigateToFirstPacketGroupLatestPacket,
-  navigateToFirstPinnedPacket,
   packetDisplayNameFromPinListItem,
   packetGroupNameFromListItem
 } from "./utils";
@@ -16,23 +14,16 @@ import {
 test.describe("Index page", () => {
   let packetGroupContainer: Locator;
   let packetGroups: Locator;
-  let pinsContainer: Locator;
-  let pins: Locator;
 
   test.beforeEach(async ({ page }) => {
     await page.goto("./");
 
     packetGroupContainer = await getPacketGroupIndexLocator(page);
-    packetGroups = await packetGroupContainer.getByRole("listitem");
+    packetGroups = packetGroupContainer.getByRole("listitem");
     expect(await packetGroups.count()).toBeGreaterThan(0);
 
-    pinsContainer = await getPinsLocator(page);
-    pins = await pinsContainer.getByRole("listitem");
-    expect(await pins.count()).toBeGreaterThan(0);
-
     // wait for list items to not be skeletal
-    await expect(await packetGroups.first().getByRole("heading")).toBeVisible();
-    await expect(await pins.first().getByRole("heading")).toBeVisible();
+    await expect(packetGroups.first().getByRole("heading")).toBeVisible();
   });
 
   test("can view packet group list", async () => {
@@ -45,13 +36,13 @@ test.describe("Index page", () => {
   });
 
   test("can filter packet groups", async ({ page }) => {
-    const firstPacketGroup = await packetGroups.first();
+    const firstPacketGroup = packetGroups.first();
     const firstPacketGroupName = await packetGroupNameFromListItem(firstPacketGroup);
-    const filterInput = await page.getByPlaceholder("Filter packet groups");
+    const filterInput = page.getByPlaceholder("Filter packet groups");
     await filterInput.fill(firstPacketGroupName);
     // wait for reset-filter button to become visible
-    await expect(await packetGroupContainer.getByLabel("reset filter")).toBeVisible();
-    const filteredGroups = await packetGroupContainer.getByRole("listitem");
+    await expect(packetGroupContainer.getByLabel("reset filter")).toBeVisible();
+    const filteredGroups = packetGroupContainer.getByRole("listitem");
     // expect to have at least one packet group remaining, and expect all to have filter term as a substring
     expect(await filteredGroups.count()).toBeGreaterThan(0);
     for (const packetGroup of await filteredGroups.all()) {
@@ -74,32 +65,54 @@ test.describe("Index page", () => {
     await expect(await getBreadcrumbLocator(page)).toHaveText(`home${displayPacketGroupName}${displayPacketId}`);
   });
 
-  test("can view list of pins", async () => {
-    (await pins.all()).forEach(async (pin) => {
-      const packetDisplayName = await packetDisplayNameFromPinListItem(pin);
-      expect(pin.getByRole("link", { name: packetDisplayName })).toBeEnabled();
+  test.describe("pinned packets", async () => {
+    let pinsContainer: Locator;
+    let pins: Locator;
 
-      const button = pin.getByRole("button");
-      const buttonName = await button.textContent();
+    test.beforeEach(async ({ page }) => {
+      pinsContainer = page.getByTestId("pins");
+      pins = pinsContainer.getByRole("listitem");
+      expect(await pins.count()).toBeGreaterThan(0);
 
-      if (buttonName && /Download artefacts/.test(buttonName)) {
-        expect(await button.isEnabled()).toBe(true);
-      } else if (buttonName && /No artefacts/.test(buttonName)) {
-        expect(await button.isEnabled()).toBe(false);
-      } else {
-        throw new Error(`Unexpected button name: ${buttonName}`);
-      }
-
-      expect(pin.getByText(/^Ran \d+ (second|minute|hour|day)s? ago$/)).toBeVisible();
+      // wait for list items to not be skeletal
+      await expect(pins.first().getByRole("heading")).toBeVisible();
     });
-  });
 
-  test("can navigate from pinned packet name link to packet page", async ({ page }) => {
-    const { packetName, packetId } = await navigateToFirstPinnedPacket(pinsContainer);
-    // wait for packet group name and latest packet id to be visible in breadcrumb
-    const displayPacketId = getReadableIdString(packetId);
-    const displayPacketGroupName = getReadableIdString(packetName); // packet group names are identical to packet names
-    await expect(await getBreadcrumbLocator(page)).toHaveText(`home${displayPacketGroupName}${displayPacketId}`);
+    test("can view list of pins", async () => {
+      (await pins.all()).forEach(async (pin) => {
+        const packetDisplayName = await packetDisplayNameFromPinListItem(pin);
+        expect(pin.getByRole("link", { name: packetDisplayName })).toBeEnabled();
+
+        const button = pin.getByRole("button");
+        const buttonName = await button.textContent();
+
+        if (buttonName && /Download artefacts/.test(buttonName)) {
+          expect(await button.isEnabled()).toBe(true);
+        } else if (buttonName && /No artefacts/.test(buttonName)) {
+          expect(await button.isEnabled()).toBe(false);
+        } else {
+          throw new Error(`Unexpected button name: ${buttonName}`);
+        }
+
+        expect(pin.getByText(/^Ran \d+ (second|minute|hour|day)s? ago$/)).toBeVisible();
+      });
+    });
+
+    test("can navigate from pinned packet name link to packet page", async ({ page }) => {
+      const firstPin = pinsContainer.getByRole("listitem").first();
+      await expect(firstPin.getByRole("heading")).toBeVisible(); // wait for text to load
+      const packetDisplayName = await packetDisplayNameFromPinListItem(firstPin);
+      const link = firstPin.getByRole("link", { name: packetDisplayName });
+      const href = await link.getAttribute("href");
+      expect(href).not.toBeNull();
+      const packetId = href?.split("/").at(-1) as string;
+      const packetName = href?.split("/").at(-2) as string;
+      await link.click();
+      // wait for packet group name and latest packet id to be visible in breadcrumb
+      const displayPacketId = getReadableIdString(packetId);
+      const displayGroupName = getReadableIdString(packetName); // packet group names are identical to packet names
+      await expect(await getBreadcrumbLocator(page)).toHaveText(`home${displayGroupName}${displayPacketId}`);
+    });
   });
 
   test("can add update read permission on packet group", { tag: TAG_STATE_MUTATE }, async ({ page }) => {
