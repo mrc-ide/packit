5611e63a7ce8b43a5c080f33ed5444bb4b8eb15b
anmol thapar <mr.anmolthapar@gmail.com>
2025-06-23 08:36:22 +0100

    fix: correct URL construction in RunnerControllerTest

--- api/app/src/test/kotlin/packit/integration/controllers/RunnerControllerTest.kt
+++ api/app/src/test/kotlin/packit/integration/controllers/RunnerControllerTest.kt
@@ -214,7 +214,7 @@ class RunnerControllerTest : IntegrationTest() {
 
         val res = restTemplate.exchange(
             "/runner/list/status?pageNumber=$pageNumber&pageSize=" +
-                    "$pageSize&filterPacketGroupName=$filterPacketGroupName",
+                "$pageSize&filterPacketGroupName=$filterPacketGroupName",
             HttpMethod.GET,
             getTokenizedHttpEntity(),
             String::class.java
