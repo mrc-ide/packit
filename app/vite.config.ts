d019b2f9e7151099300b703502464340e23d6a62
anmol thapar <mr.anmolthapar@gmail.com>
2025-07-01 10:45:26 +0100

    feat: get working version

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,9 +2,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath } from "url";
+import tailwind from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        tailwind(),
+        autoprefixer // Changed from require to import
+      ]
+    }
+  },
   build: {
     outDir: "build" // CRA's default build output
   },
