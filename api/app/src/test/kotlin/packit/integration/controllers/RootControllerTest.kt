14d876222b8e84eff36717a12086551fda23cd3d
Emma <EmmaRussellKilburn@gmail.com>
2025-08-22 11:50:11 +0100

    lint

--- api/app/src/test/kotlin/packit/integration/controllers/RootControllerTest.kt
+++ api/app/src/test/kotlin/packit/integration/controllers/RootControllerTest.kt
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus
 import packit.integration.IntegrationTest
 import kotlin.test.assertEquals
 
-class RootControllerTest: IntegrationTest() {
+class RootControllerTest : IntegrationTest() {
     @Test
     fun `root returns 200 with welcome message`() {
         val result = restTemplate.getForEntity("/", String::class.java)
