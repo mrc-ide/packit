ccb1a714c13b4c0b7aa08223684a78c6ef1b6ec1
Emma <EmmaRussellKilburn@gmail.com>
2025-10-21 16:06:16 +0100

    remove commented out code

--- api/app/src/test/kotlin/packit/integration/IntegrationTest.kt
+++ api/app/src/test/kotlin/packit/integration/IntegrationTest.kt
@@ -21,7 +21,6 @@ abstract class IntegrationTest {
         // status of 401 responses, using alternative factory fixes this
         // https://stackoverflow.com/questions/16748969
         val factory = SimpleClientHttpRequestFactory()
-        // factory.setOutputStreaming(false)
         restTemplate.restTemplate.requestFactory = factory
     }
 
